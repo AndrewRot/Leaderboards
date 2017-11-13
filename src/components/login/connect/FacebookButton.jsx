929b074f83404f30f9f27ee141a71cae27238259
Gauthierre <tvalcourt7@yahoo.com>
2017-11-13 16:17:28 -0500

    Removed old React.Component code.

--- src/components/login/connect/FacebookButton.jsx
+++ src/components/login/connect/FacebookButton.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class FacebookButton extends React.Component {
+export default class FacebookButton extends React {
    constructor(props) {
       super(props);
 
